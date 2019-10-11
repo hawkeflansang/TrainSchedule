fb2fd31016e84da9497399b29773940b4f2f26b9
Hawke Lansang <hawkeflansang@gmail.com>
2019-10-10 20:16:01 -0400

    added firebase reference to js file

---
+++ assets/trainschedule.js
@@ -0,0 +1,14 @@
+var config = {
+  apiKey: "AIzaSyCNgmYr7-h-xO6vSyUYz_ka2H3TeCAB-g0",
+  authDomain: "train-schedule-b2c0d.firebaseapp.com",
+  databaseURL: "https://train-schedule-b2c0d.firebaseio.com",
+  projectId: "train-schedule-b2c0d",
+  storageBucket: "train-schedule-b2c0d.appspot.com",
+  messagingSenderId: "487878391040",
+  appId: "1:487878391040:web:194d3bec2c5429478dafe5",
+  measurementId: "G-859JHKB570"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
